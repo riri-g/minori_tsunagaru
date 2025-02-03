5aefa4682009aa240390f243bea88b993d2994f2
riri-G <186363613+riri-g@users.noreply.github.com>
2025-02-04 00:38:37 +0900

    レスポンシブ対応

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,25 @@
 class MessagesController < ApplicationController
-
   def create
-  if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-    @message = Message.new(message_params)
-    @message.user_id = current_user.id
-    if @message.save
-        # メッセージ送信成功時の処理
+    @room = Room.find(params[:message][:room_id])
+    @message = Message.new(message_params.merge(user_id: current_user.id))
+    
+    if Entry.exists?(user_id: current_user.id, room_id: @room.id)
+      if @message.save
+        redirect_to @room
       else
-        flash[:alert] = @message.errors.full_messages.to_sentence
+        @messages = @room.messages.includes(:user)
+        @entries = @room.entries
+        @another_entry = @entries.where.not(user_id: current_user.id).first
+  
+        flash.now[:alert] = @message.errors.full_messages.join(", ")
+        render 'rooms/show', status: :unprocessable_entity
       end
     else
       flash[:alert] = "メッセージ送信に失敗しました。"
+      redirect_to rooms_path
     end
-    render :validater unless @message.save
   end
-
+  
 private
 
   def message_params
