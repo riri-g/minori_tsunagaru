9615dfa2806392b59f2e82958531fb4c9a3df4af
riri-G <186363613+riri-g@users.noreply.github.com>
2025-02-11 17:44:01 +0900

    デプロイ

--- app/controllers/debug_controller.rb
+++ app/controllers/debug_controller.rb
@@ -1,4 +1,5 @@
 class DebugController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:migrate]
     def migrate
       Rails.application.load_tasks
       Rake::Task['db:migrate'].invoke
