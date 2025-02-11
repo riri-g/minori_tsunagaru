54f0898dc76dc63fe7559ab4d5036d0db26be41b
riri-G <186363613+riri-g@users.noreply.github.com>
2025-02-11 16:03:32 +0900

    デプロイ

--- db/migrate/20250110013858_add_post_id_to_categories.rb
+++ db/migrate/20250110013858_add_post_id_to_categories.rb
@@ -1,5 +1,5 @@
 class AddPostIdToCategories < ActiveRecord::Migration[7.1]
   def change
-    add_column :categories, :post_id, :integer
+    # add_column :categories, :post_id, :integer
   end
 end
--- db/migrate/20250116164338_add_content_to_messages.rb
+++ db/migrate/20250116164338_add_content_to_messages.rb
@@ -1,5 +1,5 @@
 class AddContentToMessages < ActiveRecord::Migration[7.1]
   def change
-    add_column :messages, :content, :text
+    # add_column :messages, :content, :text
   end
 end
