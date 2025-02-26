26eb6ac445fdeb5225035e0875e453336f243a26
riri-G <186363613+riri-g@users.noreply.github.com>
2025-02-26 23:31:09 +0900

    デプロイ

--- db/migrate/20250106153615_add_category_id_to_posts.rb
+++ db/migrate/20250106153615_add_category_id_to_posts.rb
@@ -1,5 +1,5 @@
 class AddCategoryIdToPosts < ActiveRecord::Migration[7.1]
   def change
-    add_reference :categories, :post, null: false, foreign_key: true
+    add_reference :posts, :category, null: false, foreign_key: true
   end
 end
