9630af25938fa3bc1460b3bf2939a375e1c46b3f
riri-G <186363613+riri-g@users.noreply.github.com>
2025-03-14 00:37:20 +0900

    チャット

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,19 +8,19 @@ window.Stimulus   = application
 
 // DOMの読み込み完了後に実行
 document.addEventListener('DOMContentLoaded', () => {
-  // スマホ・タブレットサイズ時のみ表示されるメニューの開閉ボタンを変数に格納。
+ 
   const spMenuBtn = document.getElementById("spMenuBtn");
   
-  // メニューや開閉ボタンをラップしている要素を変数に格納。
+  
   const headerInner = document.getElementById("headerInner");
 
-  // 要素が取得できているか確認
+ 
   console.log(spMenuBtn, headerInner);
 
-  // spMenuBtnが存在する場合のみイベントを設定
+  
   if (spMenuBtn && headerInner) {
     spMenuBtn.addEventListener("click", () => {
-      // ラップ要素にactiveというクラスを付与する。
+     
       headerInner.classList.toggle("active");
     });
   }
